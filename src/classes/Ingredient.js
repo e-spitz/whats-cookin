4998c5adfeb9b52a1935c705424773d4035da3e6
Erica <>
2021-05-15 15:12:08 -0600

    Create Class files

---
+++ src/classes/Ingredient.js
@@ -0,0 +1,9 @@
+class Ingredient {
+  constructor() {
+    this.id = id;
+    this.name = name;
+    this.estimatedCostInCents = 0;
+  }
+}
+
+module.exports = Ingredient;
---
+++ src/classes/Recipe.js
@@ -0,0 +1,24 @@
+class Recipe {
+  constructor() {
+    this.id = id;
+    this.image = image;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.name = name;
+    this.tags = tags;
+  }
+
+  determineIngredients() {
+
+  }
+
+  calculateIngredientCost() {
+
+  }
+
+  returnInstructions() {
+
+  }
+}
+
+module.exports = Recipe;
