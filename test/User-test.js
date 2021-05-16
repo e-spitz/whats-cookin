3eaf62be23c50a3b8fbf922fcae927122256f8f3
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-16 11:08:43 -0600

    Add imports & function test

---
+++ test/User-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import User from '../src/classes/User';
+
+describe('User', () => {
+  it('Should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+})
