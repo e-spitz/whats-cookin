353616f9ea7e2c1739feb3aee391c95ed353ae54
Erica <>
2021-05-16 12:12:32 -0600

    Start test build out

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,21 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import recipeData from '../data/recipes-test-data';
+
 
 describe('RecipeRepository', () => {
+  let recipeRepository;
+
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository(recipeData);
+  });
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
+  it('should be an instance of RecipeRepository'), () => {
+    expect(recipeRepository).to.be.an.instanceof(RecipeRepository);
+  });
+
 })
