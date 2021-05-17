ae7f55c5456f34adf132445badc170bae0123216
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-17 14:24:55 -0600

    Add method pseudocode

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,5 +1,3 @@
-
-
 class Recipe {
   constructor() {
     this.id = id;
@@ -10,17 +8,26 @@ class Recipe {
     this.tags = tags;
   }
 
-  determineIngredients() {
+  // determineIngredients(recipeID) {
+  // // there needs to be a lot but, but this should be the last line
+  // return RecipeRepository.recipeData.ingredients
+  // }
 
-  }
+  // calculateIngredientCost(recipeID) {
+  // // there needs to be a lot more here, but this is a start
+  // const recipeID = _______.ingredientsData.id  <<<of a certain recipe>>>
+  // // ^^ we need to reference that somewhere
+  // const priceInCents = RecipeRepository.ingredients.price * _____.ingredientsData.estimatedCostInCents;
+  // const priceInDollars = priceInCents / 10;
+  // return priceInDollars;
+  // }
 
-  calculateIngredientCost() {
-
-  }
-
-  returnInstructions() {
-
-  }
-}
+//   returnInstructions(instructions) {
+//   // there needs to be a lot more here, but this is a start
+//   const instructions = _______.ingredientsData.instructions <<<of a certain recipe>>>
+//   // ^^ we need to reference that somewhere
+//   return instructions;
+//   }
+// }
 
 module.exports = Recipe;
