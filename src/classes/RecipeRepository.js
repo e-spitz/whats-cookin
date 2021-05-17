a2e5f887d1efd65abdc79a0641d215ce7e19a0bc
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-17 14:00:02 -0600

    Add method psuedocode

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -4,17 +4,37 @@ class RecipeRepository {
     this.recipeData = recipeData;
   }
 
-  filterRecipesByTag() {
+  // filterRecipesByTag(recipeTag) {
+  //   // hide all recipes that don't have a certain tag
+  //     // tags are here: this.recipeData.tags.tagName
+  //   if (
+  //     // this.filterRecipesByTag() is called is called via a tagButton being clicked
+  //   ) {
+  //     hideRecipesWithoutTag(recipeTag);
+  //     // ^^ function in dom manipulation file: scripts.js
+  //       // this function will hide recipes based on a hide function in scripts.js
+  //   }
+  // }
 
-  }
+  // filterRecipesByIngredient(recipeIngredient) {
+  //   // hide all recipes that don't have a certain ingredient
+  //     // ingredients are here: this.recipeData.tags.ingredients
+  //   if (
+  //     // this.filterRecipesByIngredient() is called via an ingredientButton being clicked
+  //   ) {
+  //     hideRecipesWithoutIngredient(recipeIngredient);
+  //   // ^^ function in dom manipulation file: scripts.js
+  //     // this function will hide recipes based on a hide function in scripts.js
+  // }
 
-  filterRecipesByIngredients() {
-
-  }
-
-  filterRecipesByName() {
-
-  }
-}
+//   filterRecipesByName(recipeName) {
+//     // hide all recipes that don't have a certain name
+//       // ingredients are here: this.recipeData.tags.name
+//     if (
+//       // this.filterRecipesByName() is called via a nameButton being clicked
+//     ) {
+//       hideRecipesWithoutName(recipeName);
+//   }
+// }
 
 export default RecipeRepository;
