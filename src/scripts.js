ba66de2f1e3ebccd0db13a139b3a0e54455d556f
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-17 17:51:32 -0600

    Add many dom elements and show/hide functions

--- src/scripts.js
+++ src/scripts.js
@@ -20,8 +20,21 @@ const menuTabBtn = document.querySelector('#menuTabBtn');
 
 // SECTIONS
 
-const sortByCourseSection = document.querySelector('#sortByCourseSection');
 const recipeSection = document.querySelector('#recipeSection');
+const rectangleGallerySection = document.querySelector('#rectangleGallerySection');
+const circleGallerySection = document.querySelector('#circleGallerySection');
+const sortByCourseHeading = document.querySelector('sortByCourseHeading');
+
+// IMAGES
+
+// categories
+
+const appetizerImg = document.querySelector('#appetizerImg');
+const breakfastImg = document.querySelector('#breakfastImg');
+const lunchImg = document.querySelector('#lunchImg');
+const dinnerImg = document.querySelector('#dinnerImg');
+const dessertImg = document.querySelector('#dessertImg');
+const sideImg = document.querySelector('#sideImg');
 
 // ***** EVENT LISTENERS *****
 
@@ -31,6 +44,15 @@ homeTabBtn.addEventListener('click', showHomeDisplay);
 browseTabBtn.addEventListener('click', showBrowseDisplay);
 favoritesTabBtn.addEventListener('click', showFavoritesDisplay);
 menuTabBtn.addEventListener('click', showMenuDisplay);
+
+// IMAGES
+
+appetizerImg.addEventListener('click', showRectangleGalleryDisplay);
+breakfastImg.addEventListener('click', showRectangleGalleryDisplay);
+lunchImg.addEventListener('click', showRectangleGalleryDisplay);
+dinnerImg.addEventListener('click', showRectangleGalleryDisplay);
+dessertImg.addEventListener('click', showRectangleGalleryDisplay);
+sideImg.addEventListener('click', showRectangleGalleryDisplay);
 
 // ***** FUNCTIONS *****
 
@@ -51,28 +73,33 @@ function show(elements) {
 // SHOW & HIDE ITEMS
 
 function showHomeDisplay() {
-  show([homeNavBar, browseTabBtn, favoritesTabBtn, menuTabBtn, sortByCourseSection]);
+  show([homeNavBar, browseTabBtn, favoritesTabBtn, menuTabBtn, sortByCourseHeading, rectangleGallerySection, circleGallerySection]);
   hide([browseNavBar, favoritesNavBar, menuNavBar, recipeNavBar, homeTabBtn]);
 }
 
 function showBrowseDisplay() {
-  show([browseNavBar, homeTabBtn, favoritesTabBtn, menuTabBtn]);
-  hide([homeNavBar, favoritesNavBar, menuNavBar, recipeNavBar, browseTabBtn, sortByCourseSection]);
+  show([browseNavBar, homeTabBtn, favoritesTabBtn, menuTabBtn, rectangleGallerySection]);
+  hide([homeNavBar, favoritesNavBar, menuNavBar, recipeNavBar, browseTabBtn, sortByCourseHeading, circleGallerySection]);
 }
 
 function showFavoritesDisplay() {
-  show([favoritesNavBar, homeTabBtn, browseTabBtn, menuTabBtn]);
-  hide([homeNavBar, browseNavBar, menuNavBar, recipeNavBar, favoritesTabBtn, sortByCourseSection]);
+  show([favoritesNavBar, homeTabBtn, browseTabBtn, menuTabBtn, rectangleGallerySection]);
+  hide([homeNavBar, browseNavBar, menuNavBar, recipeNavBar, favoritesTabBtn, sortByCourseHeading, circleGallerySection]);
 }
 
 function showMenuDisplay() {
-  show([menuNavBar, homeTabBtn, browseTabBtn, favoritesTabBtn]);
-  hide([homeNavBar, browseNavBar, favoritesNavBar, recipeNavBar, menuTabBtn, sortByCourseSection]);
+  show([menuNavBar, homeTabBtn, browseTabBtn, favoritesTabBtn, rectangleGallerySection]);
+  hide([homeNavBar, browseNavBar, favoritesNavBar, recipeNavBar, menuTabBtn, sortByCourseHeading, circleGallerySection]);
 }
 
 function showRecipeDisplay() {
   show([recipeNavBar, homeTabBtn, browseTabBtn, favoritesTabBtn, menuTabBtn, recipeSection]);
-  hide([homeNavBar, browseNavBar, favoritesNavBar, menuNavBar, sortByCourseSection]);
+  hide([homeNavBar, browseNavBar, favoritesNavBar, menuNavBar, sortByCourseHeading, circleGallerySection, rectangleGallerySection]);
+}
+
+function showRectangleGalleryDisplay() {
+  show([homeTabBtn, browseTabBtn, favoritesTabBtn, menuTabBtn, rectangleGallerySection]);
+  hide([homeNavBar, browseNavBar, favoritesNavBar, menuNavBar, sortByCourseHeading, circleGallerySection, recipeSection]);
 }
 
 console.log('Hello world');
