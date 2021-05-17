c18f0df522a9943647fa7864a27b302be3d71797
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-17 16:42:42 -0600

    Outline dom file

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,32 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 
+// ***** QUERY SELECTORS *****
 
+// NAV BARS
 
+const homeNavBar = document.querySelector('#homeNavBar');
+const allRecipeNavBar = document.querySelector('#allRecipeNavBar');
+const favoritesNavBar = document.querySelector('#favoritesNavBar');
+const menuRecipeNavBar = document.querySelector('#menuRecipeNavBar');
+const recipeNavBar = document.querySelector('#recipeNavBar');
+
+// ***** EVENT LISTENERS *****
+
+// ***** FUNCTIONS *****
+
+// SHOW & HIDE HELPER FUNCTIONS
+
+function hide(elements) {
+  elements.forEach(element => {
+    element.classList.add('hidden');
+  });
+}
+
+function show(elements) {
+  elements.forEach(element => {
+    element.classList.remove('hidden');
+  });
+}
 
 console.log('Hello world');
