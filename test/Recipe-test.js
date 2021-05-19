673916c86ec043da7f36c99894cd88b660cda792
Erica <ericaspitz522@gmail.com>
2021-05-19 09:41:03 -0400

    Remove extra file

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -2,31 +2,20 @@ import { expect } from 'chai';
 import Recipe from '../src/classes/Recipe';
 
 describe('Recipe', () => {
-  let recipe1;
-
-  beforeEach(() => {
-    recipe1 = new Recipe(678353, "https://spoonacular.com/recipeImages/678353-556x370.jpg", [{ "instruction": "Season the pork chops with salt and pepper", "number": 1}], "Maple Dijon Apple Cider Grilled Pork Chops", "starter");
-  });
-
   it('Should be a function', () => {
     expect(Recipe).to.be.a('function');
   });
 
-  it('should be an instance of Recipe', () => {
-    expect(recipe1).to.be.an.instanceof(Recipe);
-  });
-  // 
-  // it('should have an id', () => {
-  //   expect(recipe1.id).to.equal(678353);
-  // });
-
-  it.skip('should determine ingredients', () => {
+  it.skip('should be an instance of Recipe'), () => {
+    expect(recipe).to.be.an.instanceof(Recipe);
+  }
+  it.skip('should determine ingredients'), () => {
     // expect(recipe.determineIngredients().to.equal(______);
-  });
-  it.skip('should calculate ingredients by cost', () => {
+  }
+  it.skip('should calculate ingredients by cost'), () => {
     // expect(calculateIngredientCost().to.equal(______);
-  });
-  it.skip('should return instructions', () => {
+  }
+  it.skip('should return instructions'), () => {
     // expect(returnInstructions().to.equal(______);
-  });
+  }  
 })
