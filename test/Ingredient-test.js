96355839fa29f1b55c37c842ecd4b656d35fd7a3
Erica <ericaspitz522@gmail.com>
2021-05-19 20:58:09 -0400

    Add beforeEach hook

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -2,7 +2,20 @@ import { expect } from 'chai';
 import Ingredient from '../src/classes/Ingredient';
 
 describe('Ingredient', () => {
+  let ingredient;
+
+  beforeEach(() => {
+    ingredient = new Ingredient({
+      "id": 20081,
+      "quantity": {
+        "amount": 1.5,
+        "unit": "c"
+      }
+    });
+  });
+
   it('Should be a function', () => {
     expect(Ingredient).to.be.a('function');
   });
+  
 })
