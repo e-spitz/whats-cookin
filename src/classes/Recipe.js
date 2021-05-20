0413924d62b1dce19f1f0ca83fe541cd6ea4796d
Erica <ericaspitz522@gmail.com>
2021-05-19 20:51:36 -0400

    Add getCost, getInstruction tests and methods

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -16,10 +16,8 @@ class Recipe {
     const newIng = ingData.map(item => {
       return new Ingredient(item);
     });
-    // console.log(newIng); creating new ingredient instances for all ingredients
     this.ingredients.forEach(ingredient => {
       let foundIng = (newIng.find(ing => ing.id === ingredient.id));
-      // console.log(foundIng);
       ingredient.name = foundIng.name;
       ingredient.estimatedCostInCents = foundIng.estimatedCostInCents;
     });
@@ -30,32 +28,21 @@ class Recipe {
     let matchIng = this.ingredients.map(ingredient => {
       return ingredient.name;
     });
-    // console.log(matchIng);
     return matchIng;
   }
 
+  getIngredientsCost() {
+    this.findIngredients();
+    const totalCost = this.ingredients.reduce((sum, item) => {
+      sum += item.estimatedCostInCents * item.quantity.amount;
+      return sum;
+    }, 0);
+      return `$${totalCost/100}`
+  }
+
+  getRecipeInstructions() {
+    return this.instructions;
+  }
 }
-
-  // determineIngredients(recipeID) {
-  // // there needs to be a lot but, but this should be the last line
-  // return RecipeRepository.recipeData.ingredients
-  // }
-
-  // calculateIngredientCost(recipeID) {
-  // // there needs to be a lot more here, but this is a start
-  // const recipeID = _______.ingredientsData.id  <<<of a certain recipe>>>
-  // // ^^ we need to reference that somewhere
-  // const priceInCents = RecipeRepository.ingredients.price * _____.ingredientsData.estimatedCostInCents;
-  // const priceInDollars = priceInCents / 10;
-  // return priceInDollars;
-  // }
-
-//   returnInstructions(instructions) {
-//   // there needs to be a lot more here, but this is a start
-//   const instructions = _______.ingredientsData.instructions <<<of a certain recipe>>>
-//   // ^^ we need to reference that somewhere
-//   return instructions;
-//   }
-// }
 
 export default Recipe;
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -146,11 +146,26 @@ describe('Recipe', () => {
 
     expect(recipe.getIngredientsNames()).to.deep.equal(ingredientsNeeded);
   });
-  
-  it.skip('should calculate ingredients by cost'), () => {
-    // expect(calculateIngredientCost().to.equal(______);
-  }
-  it.skip('should return instructions'), () => {
-    // expect(returnInstructions().to.equal(______);
-  }
-})
+
+  it('should calculate ingredients by cost', () => {
+    expect(recipe.getIngredientsCost()).to.equal('$9.76');
+  });
+
+  it('should return instructions for making recipe', () => {
+    expect(recipe.getRecipeInstructions().length).to.equal(3);
+    expect(recipe.getRecipeInstructions()).to.deep.equal([
+      {
+        "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+        "number": 1
+      },
+      {
+        "instruction": "Add egg and vanilla and mix until combined.",
+        "number": 2
+      },
+      {
+        "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
+        "number": 3
+      },
+    ]);
+  });
+});
